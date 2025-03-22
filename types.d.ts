fa8b47af268270f1be4977a5b77b909b3b21014b
Rafael Goulart <rafaelxms@hotmail.com>
2025-03-22 11:49:06 -0300

    neon postgres configured

--- types.d.ts
+++ types.d.ts
@@ -18,6 +18,6 @@ interface AuthCredentials {
   fullName: string;
   email: string;
   password: string;
-  universityId: number;
-  universityCard: string;
+  universityRa: number;
+  universityDoc: string;
 }
